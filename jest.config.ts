8d1fc75666c2983941318ffbdeb17a4afab74986
Diana Martine <Diana_martine@outlook.com>
2024-08-13 01:36:35 -0300

    Add files via upload

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import nextJest from 'next/jest';
+import type {Config} from 'jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jsdom",
+};
+
+export default createJestConfig(config);
---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.shields.io',
+                port: '',
+                pathname: '/badge/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+                port: '',
+                pathname: '/**'
+            }
+        ]
+    },
+};
+
+export default nextConfig;
