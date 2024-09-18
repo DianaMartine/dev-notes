ced7443e5b092f5a4926dc3676e54d07d7d244f1
dianamartine <diana_martine@outlook.com>
2024-09-18 17:09:17 -0300

    feat: implements prettier

---
+++ prettier.config.js
@@ -0,0 +1,23 @@
+/**
+ *@format
+ *@type{import("prettier").Config}
+ */
+
+const config = {
+	bracketSameLine: true,
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	jsxSingleQuote: true,
+	printWidth: 80,
+	rangeEnd: Infinity,
+	requirePragma: true,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 0,
+	trailingComma: 'all',
+	useTabs: true,
+	singleAttributePerLine: true,
+	vueIndentScriptAndStyle: true,
+};
+
+module.exports = config;
